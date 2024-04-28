2db5e201b81ed4eab83c899826d7806c23e3b569
Beyza Kütük <beyzakutuk55@gmail.com>
2024-04-28 22:45:03 +0300

    Login Panel

---
+++ CassandraWebExam/CassandraWebExam/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CassandraWebExam.Models;
+
+namespace CassandraWebExam.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IDbContext _context;
+
+    public HomeController(ILogger<HomeController> logger,IDbContext dbContext)
+    {
+        _logger = logger;
+        _context = dbContext;
+    }
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Login(LoginModels loginModels)
+    {
+      bool result=  _context.LoginUser(loginModels);
+
+        if(result)
+        {
+            return RedirectToAction("UserPanel");
+        }
+        TempData["Error"] = "Hatalı kullanıcı adı veya şifre";
+
+        return View();
+    }
+    
+
+}
+
---
+++ CassandraWebExam/CassandraWebExam/Models/LoginModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CassandraWebExam.Models
+{
+	public class LoginModels
+	{
+		public string? email { get; set; }
+		public string? password { get; set; }
+
+	}
+}
+
